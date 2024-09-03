92d185f1435ed1367e8222fe001148423aa08ee0
Filpus <fil-pus@wp.pl>
2024-09-03 20:02:54 +0200

    <feat> - adding data to database

--- DataBases/addData.py
+++ DataBases/addData.py
@@ -3,26 +3,21 @@ from sqlalchemy.orm import sessionmaker
 
 
 def add_row_to_table(db_name, table_name, table, **kwargs):
-    # Tworzenie silnika połączenia z bazą danych
     gameCode = f'sqlite:///{db_name}.db'
     engine = create_engine(gameCode, echo=True)
 
-    # Inicjalizacja metadanych
     meta = MetaData()
     meta.reflect(bind=engine)
 
-    # Pobranie referencji do tabeli
     table = meta.tables.get(table_name)
     if not table:
         print(f"Table {table_name} does not exist.")
         return
 
-    # Tworzenie sesji
     Session = sessionmaker(bind=engine)
     session = Session()
 
     try:
-        # Dodanie nowego wiersza do tabeli
         insert_stmt = table.insert().values(**kwargs)
         session.execute(insert_stmt)
         session.commit()
@@ -34,13 +29,21 @@ def add_row_to_table(db_name, table_name, table, **kwargs):
         session.close()
 
 
-# Przykład użycia:
-# Dodanie nowego wiersza do tabeli 'countries'
-add_row_to_table(
-    db_name='2',
-    table_name='countries',
-    name='Poland',
-    tax=10.0,
-    gold=100.0,
-    food=50.0
-)
+def add_row_to_db(db_number, table_model, **kwargs):
+    engine = create_engine(f'sqlite:///{db_number}.db')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        new_row = table_model(**kwargs)
+
+        session.add(new_row)
+
+        session.commit()
+        print(f"Added {new_row} to {table_model.__tablename__} table.")
+    except Exception as e:
+        session.rollback()
+        print(f"Error occurred: {e}")
+    finally:
+        session.close()
